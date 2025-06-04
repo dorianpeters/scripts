167437e2ceb6e2b7c6c585fdba40e66da6c6ca7e
Dorian Peters <dpeters08@gmail.com>
2025-06-03 17:04:35 -0700

    Change to fnm and pnpm as default setup.

--- odin_git_setup.sh
+++ odin_git_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Add git repo and install latest version
-echo "***Installing git, curl, and nvm***"
+echo "*** Installing git, curl, and nvm ***"
 sudo add-apt-repository --yes ppa:git-core/ppa
 sudo apt update
 sudo apt install git curl -y
 
 # Set up git with my name, email, and options suggested by TOP
-echo "***Setting up Git for The Odin Project***"
+echo "*** Setting up Git for The Odin Project ***"
 git config --global user.name "Dorian Peters"
 git config --global user.email "dpeters08@gmail.com"
 git config --global init.defaultBranch main
@@ -17,19 +17,28 @@ git config --global pull.rebase false
 # Setup SSH Key, if needed
 if [ ! -f ~/.ssh/id_ed25519.pub ]; then
   ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" -C "dpeters08@gmail.com" -q
-  echo "***Generated SSH keys. Public key:***"
+  echo "*** Generated SSH keys. Public key: ***"
   cat ~/.ssh/id_ed25519.pub
 fi
 
 # Output name and email to make sure it's correct
-echo "***Here is the git name, email, and version ***"
+echo "*** Here is the git name, email, and version ***"
 git config --get user.name
 git config --get user.email
 git --version
-echo "***Install nvm and node  ***"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
-nvm install --lts
+
+# Install fnm and pnpm
+echo "*** Install fnm, node, and pnpm  ***"
+curl -o- https://fnm.vercel.app/install | bash
+  # Enable corepack/pnpm for all node installs
+echo 'export FNM_COREPACK_ENABLED=1' >> ~/.bashrc
+  # Add pnpm alias to bashrc
+echo 'alias pn=pnpm' >> ~/.bashrc
+  # Reload .bashrc after additions
+source ~/.bashrc 
+fnm install --lts
 echo "*** Here is the Node version ***"
 node --version
+echo "*** Here is the pnpm (pn) version ***"
+pn -v
 echo "*** Script done ***"
